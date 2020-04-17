f1d92cb55ba859861fd334fd1bbe9f1bf6a6c91d
mabumhana <m.abumhana@gmail.com>
2020-04-17 22:08:47 +0300

    delete user1_x variable

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String []args){
         int master_x = 5;
         System.out.println("master_x : "+master_x);
-        int user1_x = 6;
-        System.out.println("user1_x : "+user1_x);
+
     }
 }
